779fbfd96059e8635d9e5887556d5c180d6d35e6
zhangjianan803 <48523664+zhangjianan803@users.noreply.github.com>
2019-12-16 20:22:01 +0800

    Update search.py

--- searx/search.py
+++ searx/search.py
@@ -118,8 +118,13 @@ def search_one_offline_request_safe(engine_name, query, request_params, result_c
 
             engine_time = time() - start_time
             result_container.add_timing(engine_name, engine_time, engine_time)
+        #----- modified by zjn -----
+        #Lock->Rlock;       
             with threading.RLock():
-                engine.stats['engine_time'] += engine_time
+        #----- modified by zjn -----
+        #+= -> join().
+                engine.stats['engine_time']=join(engine_time)
+                #engine.stats['engine_time'] += engine_time
                 engine.stats['engine_time_count'] += 1
 
     except ValueError as e:
