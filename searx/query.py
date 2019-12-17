a7bfaf7b44ab6ff22edce11bb44349c618b74d95
Wenke_Huang <591852399@qq.com>
2019-12-17 08:53:36 +0800

    update the if order in query.py

--- searx/query.py
+++ searx/query.py
@@ -64,8 +64,11 @@ class RawTextQuery(object):
             parse_next = False
 
             # part does only contain spaces, skip
-            if query_part.isspace()\
-               or query_part == '':
+            # ----- modified by WenkeHuang -----
+            # after testing, find that the latter one is more likely to be true
+            # so change their position to speed up the program
+            # if query_part.isspace() or query_part == '':
+            if query_part == '' or query_part.isspace():
                 parse_next = True
                 self.query_parts.append(query_part)
                 continue
